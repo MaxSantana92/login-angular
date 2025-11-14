d09c101cec5faf5a976b0b5e8ed15f86ce53bcf8
Max Santana <maxsantana.dev@gmail.com>
2025-11-13 21:38:15 -0300

    Model user data

---
+++ src/app/interfaces/user.interface.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: number;
+  username: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  gender: string;
+  image: string;
+  // No incluyas el token aquí, solo datos del usuario
+}
