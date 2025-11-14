50f30bcd9263e24f974028e404666cd993fb57fd
Max Santana <maxsantana.dev@gmail.com>
2025-11-13 23:47:28 -0300

    Notificación Service

---
+++ src/app/services/notification.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, inject } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+  private snackBar = inject(MatSnackBar);
+
+  showError(message: string): void {
+    this.snackBar.open(message, 'Close', {
+      duration: 5000, // 5 seconds
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      panelClass: ['error-snackbar'] // Para estilos personalizados
+    });
+  }
+
+  showSuccess(message: string): void {
+    this.snackBar.open(message, 'Close', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      panelClass: ['success-snackbar']
+    });
+  }
+}
