4febd7d082066041935aa1b0c47f2b549e984cbf
Max Santana <maxsantana.dev@gmail.com>
2025-11-13 21:38:50 -0300

    Added interceptor to handle token and set Headers

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,11 +1,30 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  if (req.method === 'POST') {
-    const newReq = req.clone({
-      headers: req.headers.set('Content-Type', 'application/json'),
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const token = authService.getToken();
+
+  // Clonamos la request para agregar el header si existe el token
+  let authReq = req;
+  if (token) {
+    authReq = req.clone({
+      setHeaders: { Authorization: `Bearer ${token}` },
     });
-    return next(newReq);
   }
-  return next(req);
+
+  return next(authReq).pipe(
+    catchError((err: any) => {
+      if (err instanceof HttpErrorResponse && err.status === 401) {
+        // El token expiró o no es válido -> Logout forzado
+        authService.logout();
+        router.navigate(['/login']);
+      }
+      return throwError(() => err);
+    })
+  );
 };
