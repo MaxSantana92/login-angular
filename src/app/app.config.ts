927d207897449e8578979f9c0966c0f970cc31c9
Max Santana <maxsantana.dev@gmail.com>
2025-11-27 13:09:03 -0300

    configs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,36 +2,27 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
-  APP_INITIALIZER,
   inject,
   importProvidersFrom,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+  withIncrementalHydration,
+} from '@angular/platform-browser';
 import { authInterceptor } from './interceptors/auth.interceptor';
-import { AuthService } from './services/auth.service';
-
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideAnimationsAsync(),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
+    provideClientHydration(withEventReplay(), withIncrementalHydration()),
     provideHttpClient(withInterceptors([authInterceptor]), withFetch()),
     importProvidersFrom(MatSnackBarModule),
-    {
-      provide: APP_INITIALIZER,
-      useFactory: () => {
-        const authService = inject(AuthService);
-        return () => authService.checkAuthStatus();
-      },
-      multi: true,
-    },
   ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSnackBarModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
 })
 export class App {}
